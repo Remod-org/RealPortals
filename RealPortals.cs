a512b0795c67ff178acd435f10cb2204aa48f17f
RFC1920 <desolationoutpostpve@gmail.com>
2023-09-17 06:45:48 -0500

    Add license

--- RealPortals.cs
+++ RealPortals.cs
@@ -1,4 +1,26 @@
-﻿using Oxide.Core;
+﻿#region License (GPL v2)
+/*
+    RealPortals
+    Copyright (c) 2023 RFC1920 <desolationoutpostpve@gmail.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; version 2
+    of the License only.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+*/
+#endregion License (GPL v2)
+using Oxide.Core;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
